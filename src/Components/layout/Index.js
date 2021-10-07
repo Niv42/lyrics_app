deb3778bd4c35f135b59ed1313ba3fb71c12c108
Niv Monovich <niv313@gmail.com>
2021-10-07 21:06:41 +0300

    first commit

---
+++ src/Components/layout/Index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Tracks from '../../Components/tracks/Tracks';
+import Search from '../../Components/tracks/Search';
+
+
+const Index = () => {
+    return (
+        <>
+            <Search />
+            <Tracks />
+        </>
+    )
+}
+
+export default Index
